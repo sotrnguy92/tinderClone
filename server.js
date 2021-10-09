ac654344131a2f61175400c7ccbc7c36b4578400
sotrnguy92 <sonnguyen092@gmail.com>
2021-10-08 18:01:45 -0700

    adding axios call from front end to mongo server to populate people on cards

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cards from './models/dbCards.js'
+import cors from 'cors';
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001
-const connection_url = `mongodb+srv://admin:3lAgJSbJ1ZmOSnZo@cluster0.xxbuh.mongodb.net/tinderdb?retryWrites=true&w=majority`
+const connection_url = 'mongodb+srv://admin:3lAgJSbJ1ZmOSnZo@cluster0.xxbuh.mongodb.net/tinderdb?retryWrites=true&w=majority'
 
 // Middlewares
+app.use(express.json())
+app.use(cors())
 
 // DB Configs
-mongoose.connect(connection_url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-}, error => "hello")
+ mongoose.connect(connection_url, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+}, error => console.error(error))
 
 // API endpoints
 app.get('/', (req, res) => {
@@ -21,6 +24,27 @@ app.get('/', (req, res) => {
     res.send("HELLO CLEVER PROGRAMMER!!!")
 } )
 
+app.post('/tinder/cards', ((req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+}))
+
+app.get('/tinder/cards', ((req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+}))
 
 // Listener
 app.listen(port, () => console.log(`listening on port ${port}`))
--- tinder-clone/src/components/TinderCards.js
+++ tinder-clone/src/components/TinderCards.js
@@ -1,20 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import  "../styles/TinderCards.css";
 import TinderCard from "react-tinder-card";
+import axios from 'axios';
+import instance from "../axios.js";
+
 
 const TinderCards = () => {
 
-    const [people, setPeople] = useState([
-        {
-            name: 'Elon Musk',
-            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1699%26cropX2%3D3845%26cropY1%3D559%26cropY2%3D2704"
-        },
-        {
-            name: 'Jeff Bezos',
-            url: "https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg"
-        }
-    ]);
+    const [people, setPeople] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("http://localhost:8001/tinder/cards")
+            setPeople(req.data)
+        }
+        fetchData();
+
+    },[])
+
+    console.log(people)
     const swiped = (direction, nameToDelete) => {
         console.log("removing " + nameToDelete);
         // setLastDirection(direction);
@@ -36,7 +40,7 @@ const TinderCards = () => {
                         onCardLeftScreen={(() => outOfFrame(person.name))}
                     >
                         <div
-                        style={{backgroundImage: `url(${person.url})`}}
+                        style={{backgroundImage: `url(${person.imgUrl})`}}
                         className = "card"
                         >
                             <h3>{person.name}</h3>
